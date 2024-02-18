176a9fcf16d869a721ff7d8ed48cd2555f1093f8
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2024-02-18 08:31:47 -0500

    got vite working

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  root: 'src',
+  build: {
+    outDir:'../dist',
+    emptyOutDir: true
+  },
     plugins: [
          VitePWA({ registerType: 'autoUpdate' })
        ]
